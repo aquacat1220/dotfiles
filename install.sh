c4b8a2aac296d27c48590f326408182970da04a3
aquacat1220 <aquaminjun1220@gmail.com>
2024-01-17 16:15:58 +0900

    Fix xrdp installation again

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ else
 	unzip tmp/xrdp-installer.zip -d tmp
 	echo "[INFO] Unpacked xrdp-installer.zip under ./tmp/"
 	chmod +x tmp/xrdp-installer*.sh
-	./xrdp-installer*.sh
+	./tmp/xrdp-installer*.sh
 	echo "[INFO] Finished xrdp installation!"
 fi
 
