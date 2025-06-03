7922ccc6d76cb3ec552e6e8337b0b304a13ec3dc
aquacat1220 <155734094+aquacat1220@users.noreply.github.com>
2025-06-03 18:24:58 -0400

    feat: Install code

--- install.sh
+++ install.sh
@@ -204,6 +204,25 @@ fi
 echo
 echo
 
+if (which code > /dev/null 2>1&); then
+	echo "[INFO] code already installed, passing."
+else
+	echo "[INFO] Starting code installation!"
+	sudo apt-get install wget gpg
+	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+	sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+	echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+	rm -f packages.microsoft.gpg
+	
+	sudo apt install apt-transport-https
+	sudo apt update
+	sudo apt install code
+	echo "[INFO] Finished code installation!"
+fi
+
+echo
+echo
+
 echo "[INFO] Removing all unofficial docker packages."
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
 	(sudo apt-get remove $pkg) || true
