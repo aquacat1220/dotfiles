e458f03318ba14531bf4f3f70af1e1a708af70a2
aquacat1220 <155734094+aquacat1220@users.noreply.github.com>
2025-06-03 16:36:24 -0400

    fix: Ensure pass is installed

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,63 +25,69 @@ echo
 echo
 
 if ([ -e "../was_bootstrapped" ]); then
-  echo "[INFO] Bootstrapper already ran on this machine."
+	echo "[INFO] Bootstrapper already ran on this machine."
 else
-  sudo apt-get update
+	sudo apt-get update
+	
+	if (which git > /dev/null 2>&1); then
+		echo "[INFO] git already installed, passing."
+	else
+		echo "[INFO] Starting git installation!"
+		sudo apt-get install -y git
+		echo "[INFO] Finished git installation!"
+	fi
 
-  if (which git > /dev/null 2>&1); then
-  	echo "[INFO] git already installed, passing."
-  else
-    echo "[INFO] Starting git installation!"
-    sudo apt-get install -y git
-    echo "[INFO] Finished git installation!"
-  fi
+	echo
+	echo
+	
+	echo "[INFO] Creating gpg key. Remember the ID."
+	gpg --gen-key
+	echo "[INFO] Created gpg key."
+	
+	echo
+	echo
 
-  echo
-  echo
+	if (which pass > /dev/null 2>&1); then
+		echo "[INFO] pass already installed, passing."
+	else
+		echo "[INFO] Starting pass installation!"
+		sudo apt-get install -y pass
+		echo "[INFO] Finished pass installation!"
+	fi
 
-  echo "[INFO] Creating gpg key. Remember the ID."
-  gpg --gen-key
-  echo "[INFO] Created gpg key."
+	echo "[INFO] Initializing pass."
+	read -p "Enter the gpg ID used before: " gpg_id
+	pass init $gpg_id
+	echo "[INFO] Initialized pass."
+	
+	echo
+	echo
+	
+	echo "[INFO] Touching git configs for credentials."
+	git config --global credential.credentialStore gpg
+	echo "[INFO] Touched git configs for credentials."
+	
+	echo
+	echo
 
-  echo
-  echo
+	if (which curl > /dev/null 2>&1); then
+		echo "[INFO] curl already installed, passing."
+	else
+		echo "[INFO] Starting curl installation!"
+		sudo apt-get install -y curl
+		echo "[INFO] Finished curl installation!"
+	fi
 
-  echo "[INFO] Initializing pass."
-  read -p "Enter the gpg ID used before: " gpg_id
-  pass init $gpg_id
-  echo "[INFO] Initialized pass."
-
-  echo
-  echo
-
-  echo "[INFO] Touching git configs for credentials."
-  git config --global credential.credentialStore gpg
-  echo "[INFO] Touched git configs for credentials."
-
-  echo
-  echo
-
-
-  if (which curl > /dev/null 2>&1); then
-  	echo "[INFO] curl already installed, passing."
-  else
-  	echo "[INFO] Starting curl installation!"
-  	sudo apt-get install -y curl
-  	echo "[INFO] Finished curl installation!"
-  fi
-
-  echo
-  echo
-  
-  echo "[INFO] Starting gcm installation!"
-  curl -L https://aka.ms/gcm/linux-install-source.sh | sh
-  git-credential-manager configure
-  echo "[INFO] Finished gcm installation and configuration!"
-
-  echo
-  echo
-  
+	echo
+	echo
+	
+	echo "[INFO] Starting gcm installation!"
+	curl -L https://aka.ms/gcm/linux-install-source.sh | sh
+	git-credential-manager configure
+	echo "[INFO] Finished gcm installation and configuration!"
+	
+	echo
+	echo
 fi
 
 cd ..
