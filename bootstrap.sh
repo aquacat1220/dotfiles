3593fdf298acb641ec56b8f97a090b9fd1e0f5fc
aquacat1220 <155734094+aquacat1220@users.noreply.github.com>
2025-06-03 16:16:59 -0400

    feat: Add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/bash
+
+echo "[INFO] Starting bootstrapper!"
+
+echo
+echo
+
+echo "[INFO] Bootstrapper will exit on error."
+handle_error() {
+	echo "[ERR] An error occurred on line $1."
+	rm -rf tmp
+	exit 1
+}
+trap 'handle_error $LINENO' ERR
+
+echo
+echo
+
+echo "[INFO] Making temporary directory ./tmp/"
+mkdir tmp
+echo "[INFO] cd-ing into ./tmp/"
+cd tmp
+
+echo
+echo
+
+if ([ -e was_bootstrapped ]); then
+  echo "[INFO] Bootstrapper already ran on this machine."
+else
+  sudo apt-get update
+
+  if (which git > /dev/null 2>&1); then
+  	echo "[INFO] git already installed, passing."
+  else
+    echo "[INFO] Starting git installation!"
+    sudo apt-get install -y git
+    echo "[INFO] Finished git installation!"
+  fi
+
+  echo
+  echo
+
+  echo "[INFO] Creating gpg key. Remember the ID."
+  gpg --gen-key
+  echo "[INFO] Created gpg key."
+
+  echo
+  echo
+
+  echo "[INFO] Initializing pass."
+  read -p "Enter the gpg ID used before: " gpg_id
+  pass init $gpg_id
+  echo "[INFO] Initialized pass."
+
+  echo
+  echo
+
+  echo "[INFO] Touching git configs for credentials."
+  git config --global credential.credentialStore gpg
+  echo "[INFO] Touched git configs for credentials."
+
+  echo
+  echo
+
+
+  if (which curl > /dev/null 2>&1); then
+  	echo "[INFO] curl already installed, passing."
+  else
+  	echo "[INFO] Starting curl installation!"
+  	sudo apt-get install -y curl
+  	echo "[INFO] Finished curl installation!"
+  fi
+
+  echo
+  echo
+  
+  echo "[INFO] Starting gcm installation!"
+  curl -L https://aka.ms/gcm/linux-install-source.sh | sh
+  git-credential-manager configure
+  echo "[INFO] Finished gcm installation and configuration!"
+
+  echo
+  echo
+  
+fi
+
+cd ..
+echo "[INFO] cd-ed out of ./tmp/"
+rm -rf ./tmp
+echo "[INFO] Removed temporary directory ./tmp/"
+
+echo
+echo
+
+echo "[INFO] Finished bootstrapper!"
