f61cfa87d31753bc2bc4142b0bda5c7507c96033
aquacat1220 <aquacat1220@gmail.com>
2026-04-09 06:20:51 +0900

    feat: add missing public key cat

--- bootstrap.sh
+++ bootstrap.sh
@@ -94,6 +94,7 @@ else
 	ssh-add $key_path
 	echo "[INFO] Added key to ssh-agent."
 	echo "[ACTION] Copy the following content to https://github.com/settings/ssh/new to register the key."
+	cat "$key_path.pub"
 	read -p "[ACTION] Press Enter when ready."
 	echo "[INFO] Finished ssh key generation and registration!"
 
