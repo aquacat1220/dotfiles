a2f90ae539671c792ae074f10f2ef982af41a24a
aquacat1220 <aquacat1220@gmail.com>
2026-04-09 16:59:32 +0900

    feat: add code installation

--- install.sh
+++ install.sh
@@ -62,6 +62,25 @@ fi
 echo
 echo
 
+if (which code > /dev/null 2>&1); then
+	echo "[INFO] code already installed, passing."
+else
+	echo "[INFO] Starting code installation!"
+	sudo apt-get install -y wget gpg
+	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+	sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+	echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+	rm -f packages.microsoft.gpg
+	
+	sudo apt-get install -y apt-transport-https
+	sudo apt update
+	sudo apt-get install -y code
+	echo "[INFO] Finished code installation!"
+fi
+
+echo
+echo
+
 cd ..
 echo "[INFO] cd-ed out of ./tmp/"
 
